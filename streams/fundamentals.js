6fd72afdc51233d64da26ff05e69c961fd08e3de
William Nogueira <will.ribeiro@live.com>
2024-06-11 23:29:20 +0000

    Stream de escrita e transformação

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,4 +1,4 @@
-import { Readable } from "node:stream"
+import { Readable, Writable, Transform } from "node:stream"
 
 class OneToHundredStream extends Readable {
     index = 1
@@ -18,5 +18,23 @@ class OneToHundredStream extends Readable {
     } 
 }
 
+class InverseNumberStream extends Transform {
+    _transform(chunk, encoding, callback) {
+        const transformed = Number(chunk.toString()) * -1
 
-new OneToHundredStream().pipe(process.stdout)
+       callback(null, Buffer.from(String(transformed)))
+    }
+}
+
+class MultiplyByTenStream extends Writable {
+    _write(chunk, encoding, callback) {
+        console.log(Number(chunk.toString()) * 10)
+
+        callback()
+    }
+}
+
+
+new OneToHundredStream() // stream de leitura
+    .pipe(new InverseNumberStream()) // stream de transformação
+    .pipe(new MultiplyByTenStream()) // stream de escrita
