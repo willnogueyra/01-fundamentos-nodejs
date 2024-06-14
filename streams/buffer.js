f59f593bfc2eb885bf740b4556d20b41b1f2ff8a
William Nogueira <will.ribeiro@live.com>
2024-06-14 22:59:32 +0000

    conceito de buffers

---
+++ streams/buffer.js
@@ -0,0 +1,9 @@
+const buf = Buffer.from("hexadecimal")
+
+console.log(buf)
+
+/*
+    O Buffer é um objeto do Node.js que é utilizado para armazenar dados em memória temporária,
+    meio que em um pedacinho dela para ser mais exato. Ele é muito utilizado em operações que envolvem 
+    manipulação de dados binários, como justamente o tema do curso -> a criptografia de informações
+*/
