31e813dc112834204f2624bb21c20a7be70e93c6
William Nogueira <will.ribeiro@live.com>
2024-06-14 21:34:01 +0000

    Requisições de informações em JSON usando Buffer

--- src/server.js
+++ src/server.js
@@ -2,9 +2,22 @@ import http from "node:http"
 
 const users = []
 
-const server = http.createServer((request, response) => {
+const server = http.createServer(async (request, response) => {
     const { method, url } = request
 
+    const buffers = []
+
+    // garanti que a aplicação não executa a lógica antes de finalizar a leitura da stream por completo
+    for await (const chunk of request) {
+        buffers.push(chunk)
+    }
+    
+    try {
+        request.body = JSON.parse(Buffer.concat(buffers).toString())
+    } catch {
+        request.body = null
+    }
+    
     if (method === 'GET' && url === '/users') {
         return response
             .setHeader('Content-type', 'application/json') // formatação de metadados 
@@ -12,23 +25,24 @@ const server = http.createServer((request, response) => {
     } 
 
     if(method === 'POST' && url === '/users'){
+        const { name, email } = request.body
+
         users.push({
             id: 1,
-            name: 'William',
-            email: 'will.ribeiro@live.com'
+            name,
+            email
         })
 
         return response.writeHead(201).end()
     } 
 
     return response.writeHead(404).end("Não Encontrado")
-
-    
 })
 
 server.listen(3333)  // localhost:3333
 
-/*  
+/*  ANOTAÇÕES
+
     CRUD =>  Create (criar), Read (ler), Update (atualizar) e Delete (apagar)
 
     HTTP
