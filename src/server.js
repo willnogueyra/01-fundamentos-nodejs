e81cf02a5cca3d9d4b830deea8211500f83e1d13
William Nogueira <will.ribeiro@live.com>
2024-06-17 22:52:07 +0000

    Criação de rota DELETE para remoção de registro com regEx

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import http from "node:http"
 import { json } from "./middlewares/json.js"
-import {routes} from './middlewares/routes.js'
+import {routes} from './routes.js'
 
 const server = http.createServer(async (request, response) => {
     const { method, url } = request
@@ -8,10 +8,14 @@ const server = http.createServer(async (request, response) => {
     await json(request, response)
     
     const route = routes.find(route => {
-        return route.method === method && route.path === url
+        return route.method === method && route.path.test(url)
     })
 
     if (route) {
+        const routeParams = request.url.match(route.path)
+
+        request.params = {... routeParams.groups }
+
         return route.handler(request,response)
     }
 
